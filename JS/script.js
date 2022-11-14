33b6e61b51a335124c3f5d4307e895a5909260b6
rubymaxioned <axioned0482022>
2022-11-14 10:59:07 +0530

    adding key

---
+++ JS/script.js
@@ -0,0 +1,45 @@
+var keySpan = document.querySelector(".e-key");
+var codeSpan = document.querySelector(".e-code");
+var keycodeSpan = document.querySelector(".e-keycode");
+var input = document.querySelector(".input");
+
+var a = document.querySelector(".press");
+var b = document.querySelector(".key");
+
+input.addEventListener("keyup", function(e) {
+    keySpan.innerText = e.key;
+    codeSpan.innerText = e.keyCode;
+    keycodeSpan.innerText = e.code;
+})
+
+a.addEventListener("click",myFunction);
+function myFunction(){
+    a.classList.add('hide');
+    b.classList.add('show');
+}
+
+
+
+
+
+
+
+
+
+
+// var a = document.querySelector(".press");
+// var b = document.querySelector(".key");
+// // console.log(b);
+// a.addEventListener("click",myFunction);
+// function myFunction(){
+//     a.classList.add('hide');
+//     b.classList.add('show');
+// }
+// var c = document.querySelectorAll('.key-name');
+// // c.addEventListener('onkeypress',pressKey);
+// // console.log(c);
+// c.forEach(function(list){
+//     list.addEventListener("keypress",function(event) {
+//         console.log(e);
+//     })
+// })
