208aec68958269936762b6f918e3a381e64004fe
rubymaxioned <axioned0482022>
2022-11-29 10:32:49 +0530

    changed variable name

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,16 @@ var keySpan = document.querySelector(".e-key");
 var codeSpan = document.querySelector(".e-code");
 var keycodeSpan = document.querySelector(".e-keycode");
 var input = document.querySelector(".input");
-var a = document.querySelector(".press span");
-var b = document.querySelector(".key");
+var press = document.querySelector(".press span");
+var key = document.querySelector(".key");
 
 window.addEventListener("keydown", myFunction);
 function myFunction(e) {
     keySpan.innerText = e.key;
     codeSpan.innerText = e.keyCode;
     keycodeSpan.innerText = e.code;
-    a.classList.add('hide');
-    b.classList.add('show');
+    press.classList.add('hide');
+    key.classList.add('show');
 }
 
 
